32b1fb56d3a90f8bd6ddb516526a3145922f51ae
gianlucampos <gianlucampos97@gmail.com>
2021-04-03 20:31:10 -0300

    Adicionando footer na tabela

--- src/components/MusicaFilter.tsx
+++ src/components/MusicaFilter.tsx
@@ -5,41 +5,43 @@ export function MusicaFilter() {
     <div className={"containerTable"}>
       <div className={"tableTitle"}>
         <p> Músicas </p>
-        <table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Música</th>
-              <th>Album</th>
-              <th>Artista</th>
-              <th>Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr key={1}>
-              <td>1</td>
-              <td>All the small things</td>
-              <td>The Enema of State</td>
-              <td>Blink-182</td>
-              <td>Excluir</td>
-            </tr>
-            <tr key={2}>
-              <td>1</td>
-              <td>All the small things</td>
-              <td>The Enema of State</td>
-              <td>Blink-182</td>
-              <td>Excluir</td>
-            </tr>
-            <tr key={3}>
-              <td>1</td>
-              <td>All the small things</td>
-              <td>The Enema of State</td>
-              <td>Blink-182</td>
-              <td>Excluir</td>
-            </tr>
-          
-          </tbody>
-        </table>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Música</th>
+            <th>Album</th>
+            <th>Artista</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr key={1}>
+            <td>1</td>
+            <td>All the small things</td>
+            <td>The Enema of State</td>
+            <td>Blink-182</td>
+            <td>Excluir</td>
+          </tr>
+          <tr key={2}>
+            <td>1</td>
+            <td>All the small things</td>
+            <td>The Enema of State</td>
+            <td>Blink-182</td>
+            <td>Excluir</td>
+          </tr>
+          <tr key={3}>
+            <td>1</td>
+            <td>All the small things</td>
+            <td>The Enema of State</td>
+            <td>Blink-182</td>
+            <td>Excluir</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className={"tableBottom"}>
+        <div className={"hintText"}>Exibindo 5 de 25 resultados</div>
       </div>
     </div>
   );
