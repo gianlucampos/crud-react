30de7d6029efdd19ec88c99b047b888b9dfd99e0
gianlucampos <gianlucampos97@gmail.com>
2021-04-02 22:44:41 -0300

    Adicionado titulo da tabela

--- src/components/MusicaFilter.tsx
+++ src/components/MusicaFilter.tsx
@@ -3,7 +3,9 @@ import '../styles/components/MusicaFilter.css';
 export function MusicaFilter() {
   return (
     <div className={"containerTable"}>
-      
+      <div className={"tableTitle"}>
+        <p> Músicas </p>
+      </div>
     </div>
   );
 }
