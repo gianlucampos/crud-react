900bbc3ba7409bea87e8bd340c34b9c76b0e235a
gianlucampos <gianlucampos97@gmail.com>
2021-04-02 23:44:53 -0300

    Ajuste layout tabela

--- src/components/MusicaFilter.tsx
+++ src/components/MusicaFilter.tsx
@@ -5,6 +5,41 @@ export function MusicaFilter() {
     <div className={"containerTable"}>
       <div className={"tableTitle"}>
         <p> Músicas </p>
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Música</th>
+              <th>Album</th>
+              <th>Artista</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr key={1}>
+              <td>1</td>
+              <td>All the small things</td>
+              <td>The Enema of State</td>
+              <td>Blink-182</td>
+              <td>Excluir</td>
+            </tr>
+            <tr key={2}>
+              <td>1</td>
+              <td>All the small things</td>
+              <td>The Enema of State</td>
+              <td>Blink-182</td>
+              <td>Excluir</td>
+            </tr>
+            <tr key={3}>
+              <td>1</td>
+              <td>All the small things</td>
+              <td>The Enema of State</td>
+              <td>Blink-182</td>
+              <td>Excluir</td>
+            </tr>
+          
+          </tbody>
+        </table>
       </div>
     </div>
   );
