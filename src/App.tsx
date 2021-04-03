c0b79083ff83d7b8cec29b124960a81b208c0f25
gianlucampos <gianlucampos97@gmail.com>
2021-04-02 21:48:26 -0300

    Tentando criar tabela centralizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { HashRouter as Router } from 'react-router-dom';
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
+import { MusicaFilter } from './components/MusicaFilter';
 import './styles/global.css';
 
 function App() {
   return (
     <div className="pageContainer">
+      <Header />
       <div className="contentWrap">
         <Router basename={process.env.PUBLIC_URL}>
-          <Header />
+          <MusicaFilter />
         </Router>
       </div>
       <Footer />
---
+++ src/components/MusicaFilter.tsx
@@ -0,0 +1,9 @@
+import '../styles/components/MusicaFilter.css';
+
+export function MusicaFilter() {
+  return (
+    <div className={"containerTable"}>
+      
+    </div>
+  );
+}
