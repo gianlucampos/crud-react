6334c248a578c43cb6f8494e361d17e77d18ef18
gianlucampos <gianlucampos97@gmail.com>
2021-04-04 16:54:36 -0300

    Adicionado css do form de cadastro

--- src/components/MusicaCad.tsx
+++ src/components/MusicaCad.tsx
@@ -1,7 +1,21 @@
+import '../styles/components/MusicaCad.css'
+
 export function MusicaCad() {
   return (
-    <div>
-      Cadastro de Música
+    <div className={"container"}>
+      <div className={"box"}>
+        <div className={"formCad"}>
+          <h2>Cadastro</h2>
+          <label> Música </label>
+          <input placeholder="Nome da música" name="nomeMusica" />
+          <br/>
+          <label> Artista </label>
+          <input placeholder="Nome da música" name="nomeMusica" />
+          <br/>
+          <label> Album </label>
+          <input placeholder="Nome da música" name="nomeMusica" />
+        </div>
+      </div>
     </div>
   );
 }
